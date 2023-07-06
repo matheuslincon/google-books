ed0d5abe2a7af5d15a4553e23048fcf6fc9ba117
Matheus Andrade <matheuslincon50@gmail.com>
2023-07-06 12:46:34 +1000

    added loading state when searching

--- src/containers/BookList/BookList.jsx
+++ src/containers/BookList/BookList.jsx
@@ -2,24 +2,35 @@ import { useEffect, useState } from 'react';
 import Book from '../../components/Book/Book.jsx';
 import style from './BookList.module.scss';
 import { getBooksBySearchValue } from '../../services/fetchData.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 
 const BookList = ({ search }) => {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchResult = async (search) => {
-    const booksData = await getBooksBySearchValue(search)
-    setBooks(booksData);
+    setLoading(true)
+    try {
+      const booksData = await getBooksBySearchValue(search)
+      setBooks(booksData);
+    } catch(e) {
+      console.log(e)
+    } finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     if(search){
-      fetchResult(search);
-    }
+        fetchResult(search)
+      }
   },[search])
 
   return (
     <>
+      <p className={style.search_text}>{loading && <FontAwesomeIcon icon={faSpinner} spin className={style.icon}/>}</p>
       <p className={style.search_text}>{!books && `No results found for: ${search}`}</p>
       <p className={style.search_text}>{books && books.length > 0 && `Showing results for: ${search}`}</p>
       <section className={style.list}>
